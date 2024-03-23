f08be81557b1deddefe070178a967aae19f30d27
xiaolei <baimax9631@163.com>
2024-03-24 00:39:46 +0800

    新建项目文件

---
+++ gateway/src/main/java/com/taco/gateway/controller/HomeController.java
@@ -0,0 +1,12 @@
+package com.taco.gateway.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+    @GetMapping("")
+    public String home() {
+        return "Home";
+    }
+}
